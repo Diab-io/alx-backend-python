f29f77226dcf4f6c823398eeff86014c69169553
Odimayo David <odimayodavid7@gmail.com>
2023-03-30 22:00:19 +0100

    Added file

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function takes float returns float"""
+
+
+def make_multiplier(multiplier: float) -> float:
+    """takes float, returns float, multiplier*multiplier"""
+    return multiplier*multiplier
