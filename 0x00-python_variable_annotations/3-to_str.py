946218dea83984d1d7ec4f3dd939a67a89cea84b
Odimayo David <odimayodavid7@gmail.com>
2023-03-30 21:17:31 +0100

    Added file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A typed-annotated function that floors the value passed """
+
+
+def to_str(n: float) -> str:
+    """ returns string representation of n """
+    return str(n)
