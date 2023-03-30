acb9fe71d69c1c7f1d0cf7e77bd399b8e7f13dda
Odimayo David <odimayodavid7@gmail.com>
2023-03-30 21:10:04 +0100

    Added files

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" A typed-annotated function that concats 2 strings """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concats two strings """
+    return "{}{}".format(str1, str2)
