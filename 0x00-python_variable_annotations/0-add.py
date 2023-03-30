3c145c753a8da4358f7b3385b2d4f1301a25d224
Odimayo David <odimayodavid7@gmail.com>
2023-03-30 21:06:06 +0100

    Updated file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""A type-annotated function that returns the sum of two floats """
 
 def add(a: float, b: float) -> float:
+    """adds two floats and return sum as float"""
     return a + b
