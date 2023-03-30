d6b0438e59eb221bf0e2edcd12949d6477e92d90
Odimayo David <odimayodavid7@gmail.com>
2023-03-30 21:53:08 +0100

    Added file

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Type-annotated function that has params of different types"""
-
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple that contains a string and the square of the 2nd param"""
-    return (k, v**2)
+    return k, v**2
