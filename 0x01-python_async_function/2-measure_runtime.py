b2d622ce0af1f087d92ae01433b4c26558eef2b7
Odimayo David <odimayodavid7@gmail.com>
2023-04-05 19:00:53 +0100

    Added files

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" program that measured the runtime of the wait_n """
+
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_display: int) -> float:
+    """ function determines the runtime of the imported wait_n """
+    start_time = time.perf_counter()
+    asyncio.run(wait_n(n, max_display))
+    total_time = time.perf_counter() - start_time
+    return total_time / n
