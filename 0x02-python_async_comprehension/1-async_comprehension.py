541e81cdbfb601b8ea9d7be441cbdcd9e4f9f1cf
Odimayo David <odimayodavid7@gmail.com>
2023-04-05 23:26:28 +0100

    Added file

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """ A coroutine that takes no argument """
 
 import asyncio
+from typing import List
 
 generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """returns numbers comprehended from the gen."""
-    comps = [comp async for comp in generator()]
-    return comps
+    return [comp async for comp in generator()]
