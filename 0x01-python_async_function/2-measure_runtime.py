7da5cc4064dcb4f69398a4a117f9cb06f5742a5e
Odimayo David <odimayodavid7@gmail.com>
2023-04-05 19:03:55 +0100

    Added files

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,9 +7,9 @@ import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-def measure_time(n: int, max_display: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """ function determines the runtime of the imported wait_n """
     start_time = time.perf_counter()
-    asyncio.run(wait_n(n, max_display))
+    asyncio.run(wait_n(n, max_delay))
     total_time = time.perf_counter() - start_time
     return total_time / n
