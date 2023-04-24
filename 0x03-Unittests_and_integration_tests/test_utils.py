4a851bf5812c0e03cd2346035cb1822122ff80e2
Odimayo David <odimayodavid7@gmail.com>
2023-04-24 16:12:16 +0100

    Updated test_utils to check if a keyerror exception is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,5 +13,14 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """The actual method that tests the function"""
+        """Test method to check if the expected output is gotten"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test to check if a keyError is raised"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
