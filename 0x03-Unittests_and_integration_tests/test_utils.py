7eed4064f415fd4594fea606df3ebea38ad7b576
Odimayo David <odimayodavid7@gmail.com>
2023-07-31 19:24:08 +0100

    Created a test case that checks if a key error is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,13 @@ class TestAccessNestedMap(unittest.TestCase):
                                expected: Union[Dict, int]):
         """ Testing output of the accessmap function """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Method for testing that a keyerror is raised
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
