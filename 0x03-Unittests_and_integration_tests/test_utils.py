bcf39303c3d9e75c2609323d0239a2978518962c
Odimayo David <odimayodavid7@gmail.com>
2023-08-04 23:16:51 +0100

    Added docstrings

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -53,8 +53,13 @@ class TestGetJson(unittest.TestCase):
 
 
 class TestMemoize(unittest.TestCase):
+    """
+    Used for testing memoize
+    """
     def test_memoize(self):
-        """"""
+        """
+        Method used to test memoize output
+        """
         class TestClass:
 
             def a_method(self):
@@ -63,11 +68,9 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-        with patch.object(TestClass,
-                          'a_method',
-                          return_value=lambda: 42) as mock_a_method:
+        with patch.object(TestClass, 'a_method') as mock_a_method:
             test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 42)
-            self.assertEqual(test_class.a_property(), 42)
+            test_class.a_property()
+            test_class.a_property()
 
-            mock_a_method.assert_called_once()
+            mock_a_method.assert_called_once
