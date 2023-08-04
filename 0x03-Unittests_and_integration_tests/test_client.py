fd5b7263238e0f5207bfbb0d561c097c959bf303
Odimayo David <odimayodavid7@gmail.com>
2023-08-04 23:54:06 +0100

    Created tests for client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,27 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from client import GithubOrgClient
+from typing import Dict
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Test org in GithubOrgClient
+    """
+    @parameterized.expand([
+        ('google', {'login': "google"}),
+        ('abc', {'login': "abc"})
+    ])
+    @patch("client.get_json")
+    def test_org(self, org: str, res: Dict, mock_org):
+        """
+        tests output of org
+        """
+        mock_org.return_value = MagicMock(return_value=res)
+        gh_client = GithubOrgClient(org)
+        self.assertEqual(gh_client.org(), res)
+
+        mock_org.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
