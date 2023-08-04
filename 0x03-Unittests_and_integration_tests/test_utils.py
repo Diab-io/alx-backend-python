f4f89acd9514a3acf105e13c38856217390dbbee
Odimayo David <odimayodavid7@gmail.com>
2023-08-04 23:02:16 +0100

    Tested the memoize decorator from utils doesn't run a method twice when the same arg was passsed

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 from unittest.mock import Mock, patch
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -50,3 +50,22 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(res, test_payload)
 
             mock_request_get_obj.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        """"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+
+            assert mock_a_method.call_count == 1
