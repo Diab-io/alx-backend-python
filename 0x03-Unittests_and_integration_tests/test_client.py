2d439f4e6f5241b9da86d4e0d178476d25741859
Odimayo David <odimayodavid7@gmail.com>
2023-08-05 10:23:27 +0100

    implemented test for the get_repo _url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -37,3 +37,53 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient("google")._public_repos_url,
                 "https://api.github.com/users/google/repos",
             )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json: MagicMock) -> None:
+        """ Test for the pucblic repo method """
+        test_payload = {
+            'repos_url': "https://api.github.com/users/amazon/repos",
+            'repos': [
+                {
+                    "id": 432248,
+                    "name": "amazon.aws",
+                    "owner": {
+                        "login": "amazon",
+                        "id": 322234,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/amazon/amazon.aws",
+                    "created_at": "2022-07-23T00:24:37Z",
+                    "updated_at": "2023-11-15T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 8,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 54983945,
+                    "name": "giftshop",
+                    "private": False,
+                    "owner": {
+                        "login": "amazon",
+                        "id": 347567443,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/amazon/giftshop",
+                    "created_at": "2017-06-01T22:52:33Z",
+                    "updated_at": "2023-04-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 10,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mock_json.return_value = test_payload["repos"]
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_pub_url:
+            mock_pub_url.return_value = test_payload["repos_url"]
+            self.assertAlmostEqual(
+                GithubOrgClient("amazon").public_repos(),
+                ['amazon.aws', 'giftshop']
+            )
+            mock_pub_url.assert_called_once()
+        mock_json.assert_called_once()
